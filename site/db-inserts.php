2f93c8e4b400b81f5a35b8b7045ad3d7e6fe5993
seancrowe5 <Sean Crowe>
2014-06-27 09:54:30 -0500

    added back in error check and insert

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -15,6 +15,9 @@ $twilio_num = '+12243741995';  //twilio phone number
 $sql="  INSERT INTO user (f_name, phone) 
         VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
+if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
+mysqli_close($con);
+
 ////Send confirmation text////
 /////////////////////////////
 $sms = $client->account->messages->sendMessage(
