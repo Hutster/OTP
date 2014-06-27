931645583bc26a4d74f081c5e9683f480e7fea76
seancrowe5 <Sean Crowe>
2014-06-27 09:16:49 -0500

    made code look like english

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -1,12 +1,12 @@
 <?php
 
-$phonetwil = $_SESSION[phone];               //grabs the number using TWILIO api
-$phonetwil = chr(43) . "1" . $phonetwil;    //add a +1 in front of the phone to make twilio happy
+$user_phone = $_SESSION[phone];               //grabs the number using session var
+$user_phone = chr(43) . "1" . $phonetwil;    //add a +1 in front of the phone to make twilio happy
 
-require "dbconnect.php";                    //get credentials for mysql pull
+require "config/dbconnect.php";                    //get credentials for mysql pull
 
 $sql="  INSERT INTO user (f_name, phone) 
-        VALUES ('$_SESSION[first]','$phonetwil')"; //inserts in user table with first name and phone from landing page
+        VALUES ('$_SESSION[first]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
 if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
 mysqli_close($con);
