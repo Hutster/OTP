42267b4139f24aea96a5e32138971195a7f4f4eb
thomasgorczynski <me@thomasgorczynski.com>
2014-06-27 10:39:26 -0400

    change from session to post

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -1,12 +1,12 @@
 <?php
 
-$user_phone = $_SESSION[phone];               //grabs the number using session var
+$user_phone = $_POST[phone];               //grabs the number using session var
 $user_phone = chr(43) . "1" . $user_phone;    //add a +1 in front of the phone to make twilio happy
 
 require "config/db-connect.php";                    //get credentials for mysql pull
 
 $sql="  INSERT INTO user (f_name, phone) 
-        VALUES ('$_SESSION[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
+        VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
 if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
 mysqli_close($con);
