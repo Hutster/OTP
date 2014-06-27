debc0a069c1b0300a2d47827838529f6b610a184
seancrowe5 <Sean Crowe>
2014-06-27 09:46:56 -0500

    twilio updates

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -2,18 +2,21 @@
 
 require "twilio/Services/Twilio.php"; //twilio library
 require 'config/db-connect.php';        //databse connect
-require 'config/twilio-connect.php';    //twilio credentials
+require 'config/twilio-connect.php';    //twilio credentials...not sure if this will work
 
-//set variable
+////set variables////
+////////////////////
 $user_phone = $_POST[phone];               //grabs the number using session var
 $user_phone = chr(43) . "1" . $user_phone;    //add a +1 in front of the phone to make twilio happy
 $twilio_num = '+12243741995';  //twilio phone number
 
-//insert  in database
+////insert user info in database////
+///////////////////////////////////
 $sql="  INSERT INTO user (f_name, phone) 
         VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
-//Send confirmation text
+////Send confirmation text////
+/////////////////////////////
 $sms = $client->account->messages->sendMessage(
             $twilio_num, $number, "Hey dude thanks for signing up with OTP! Respond with 'This is dope' to confirm your subsciption.");
  
