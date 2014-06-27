6aa0eac43da3d4249413bca5a16dbc8f301b2c92
seancrowe5 <Sean Crowe>
2014-06-27 09:44:37 -0500

    combined ams into same file as inserts

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -1,13 +1,22 @@
 <?php
 
+require "twilio/Services/Twilio.php"; //twilio library
+require 'config/db-connect.php';        //databse connect
+require 'config/twilio-connect.php';    //twilio credentials
+
+//set variable
 $user_phone = $_POST[phone];               //grabs the number using session var
 $user_phone = chr(43) . "1" . $user_phone;    //add a +1 in front of the phone to make twilio happy
+$twilio_num = '+12243741995';  //twilio phone number
 
-require "config/db-connect.php";                    //get credentials for mysql pull
-
+//insert  in database
 $sql="  INSERT INTO user (f_name, phone) 
         VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
-if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
-mysqli_close($con);
+//Send confirmation text
+$sms = $client->account->messages->sendMessage(
+            $twilio_num, $number, "Hey dude thanks for signing up with OTP! Respond with 'This is dope' to confirm your subsciption.");
+ 
+echo "Sent message to $name "; // Display a confirmation message on the screen
+
 ?>
