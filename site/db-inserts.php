d9920182863e4d170ecc907389d608a7164fefae
seancrowe5 <Sean Crowe>
2014-06-27 09:17:49 -0500

    edit

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -6,7 +6,7 @@ $user_phone = chr(43) . "1" . $phonetwil;    //add a +1 in front of the phone to
 require "config/dbconnect.php";                    //get credentials for mysql pull
 
 $sql="  INSERT INTO user (f_name, phone) 
-        VALUES ('$_SESSION[first]','$user_phone')"; //inserts in user table with first name and phone from landing page
+        VALUES ('$_SESSION[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
 if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
 mysqli_close($con);
