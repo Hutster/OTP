78667efbd0632b5498ffdf767206a0fd21ea1907
thomasgorczynski <me@thomasgorczynski.com>
2014-07-05 14:35:30 -0400

    Lets reply with the same message now

--- site/sms-hello-monkey.php
+++ site/sms-hello-monkey.php
@@ -1,7 +1,12 @@
 <?php
 	header("content-type: text/xml");
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+
+    $phone = $_REQUEST['From'];
+	$message = $_REQUEST['Body'];
 ?>
 <Response>
-	<Message>Hello, Mobile Monkey</Message>
+	<Message>Hello <?php echo $phone; ?>, your message was <?php echo $message; ?></Message>
 </Response>
+
