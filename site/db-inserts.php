8abc1ee37d7616f57d71f2621c5b4cdfa79291cc
seancrowe5 <Sean Crowe>
2014-07-05 16:40:47 -0400

    updated file to reflect database update

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -11,7 +11,7 @@ $user_phone = chr(43) . "1" . $user_phone;    //add a +1 in front of the phone t
 
 ////insert user info in database////
 ///////////////////////////////////
-$sql="  INSERT INTO user (f_name, phone) 
+$sql="  INSERT INTO user (user_fname, user_phone) 
         VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
 
 if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
