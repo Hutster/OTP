2039b00c08fd0d2e7d15dbb637cd0b13d95cf603
thomasgorczynski <me@thomasgorczynski.com>
2014-07-22 11:37:44 -0400

    simple changes

--- site/db-inserts.php
+++ site/db-inserts.php
@@ -8,11 +8,12 @@ require 'config/twilio-connect.php';    //twilio credentials...not sure if this 
 ////////////////////
 $user_phone = $_POST[phone];               //grabs the number using session var
 $user_phone = chr(43) . "1" . $user_phone;    //add a +1 in front of the phone to make twilio happy
+$user_fname = $_POST[name];                  // grabs the user's nume
 
 ////insert user info in database////
 ///////////////////////////////////
 $sql="  INSERT INTO user (user_fname, user_phone) 
-        VALUES ('$_POST[name]','$user_phone')"; //inserts in user table with first name and phone from landing page
+        VALUES ('$user_fname','$user_phone')"; //inserts in user table with first name and phone from landing page
 
 if (!mysqli_query($con,$sql)) {die('Error: ' . mysqli_error($con));}
 mysqli_close($con);
@@ -20,8 +21,8 @@ mysqli_close($con);
 ////Send confirmation text////
 /////////////////////////////
 $sms = $client->account->messages->sendMessage(
-            $twilio_num, $user_phone, "Hey " . $name . ", thanks for signing up with OTP! Respond with 'This is dope' to confirm your subsciption.");
+            $twilio_num, $user_phone, "Hey " . $userfname . ", thanks for signing up with OTP! Respond with 'This is dope' to confirm your subsciption.");
  
-echo "Sent message to $name "; // Display a confirmation message on the screen
+echo "Sent message to " . $userfname . "!"; // Display a confirmation message on the screen
 
 ?>
