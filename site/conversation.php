d8875d196c3379e59178a675fefbb69350250f37
seancrowe5 <Sean Crowe>
2014-07-24 15:16:30 -0400

    REQUIREMENTS FOR DASHBOARD

--- site/conversation.php
+++ site/conversation.php
@@ -1,14 +1,10 @@
 <?php
 require 'config/db-connect.php';        //databse connect
+$id = $_GET['id']; //GET THE USER ID FROM THE URL SEND FROM USERS.PHP
 
-
-$id = $_GET['id'];
-    
-
-////some of the below can be replaced with: getMessages()////
-////ADD IN A CONCATENATE FUNCTION IF TIMESTAMPS ARE THE SAME////
-
-//if there are no syntax errors then execute
+///////////////////////
+///DISPLAY MESSAGES//// <- THIS SHOULD EVENTUALLY GO IN IT'S OWN FIXED BOX
+///////////////////////
 if($result = $con->query("SELECT * FROM messages WHERE sender = '$id' OR recipient = '$id' ORDER BY timestamp ASC")){
     if($result->num_rows){                          //if the query has a result, then dislpay data
         while($rows = $result->fetch_assoc()){      //loop through result and display mesages
@@ -18,6 +14,18 @@ if($result = $con->query("SELECT * FROM messages WHERE sender = '$id' OR recipie
         }  
     }
 }
+
+/////////////////////////////////////
+///INPUT FIELD WITH REPLY BUTTON///// <- ADD THIS BELOW THE FIXED BOX WITH MESSAGES IN IT
+/////////////////////////////////////
+    
+//1. OTP EMPLOYEE TYPES REPLY AND HITS THE SUBMIT BUTTON
+//2. THAT MESSAGE IS SENT TO THE USER VIA TWILLIO NUMBER: +1-224-374-1995
+//3. THE MESSAGE SHOWS INSERTED INTO DATABASE
+//4. THE MESSAGE APPEARS IN THE ABOVE BOX WITHOUT HAVING TO REFRESH
+//5. USER REPLIES AND MESSAGE APPEARS IN BOX WITHOUT REFRESHING
+
+
 
 
 ?>
