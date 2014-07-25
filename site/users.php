164deed46705205cc09f1f220f43b1d4335aa76a
thomasgorczynski <me@thomasgorczynski.com>
2014-07-24 21:44:50 -0400

    users?

--- site/users.php
+++ site/users.php
@@ -8,7 +8,7 @@
             while($rows = $result->fetch_assoc()){      //loop through result and display mesages
                 $userID = $rows['user_id'];
                 $userName = $rows['user_fname'];
-                echo $userID. ': <a href="http://stugl.com/conversation.php?id=' . $userID . '"> '. $userName . '</a></br>';
+                echo $userID. ': <a href="http://stugl.com/conversation11.php?id=' . $userID . '"> '. $userName . '</a></br>';
 
         }  
     }
