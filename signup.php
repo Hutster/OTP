9f11e8456c705c4963eb65c795b2853c5ffd1fd8
seancrowe5 <Sean Crowe>
2014-09-26 12:13:22 -0400

    new stuff?

---
+++ signup.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+<meta charset="utf-8">
+<!-- If you delete this meta tag World War Z will become a reality -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<title>Alice | Off Track Planet</title>
+
+<!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/foundation.css">
+
+<!-- If you are using the gem version, you need this only -->
+<link rel="stylesheet" href="css/app.css">
+
+<script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+<div class="signup">
+  <div class="row">
+    <div class="small-12 columns">
+      <img id="logo" src="/img/otp_logo_white.svg"/>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="small-12 columns">
+      <h1 id="title_signup">Off Track Planet</h1>
+    </div>
+  </div>
+  
+  <div class="row">
+    <div class="small-12 columns">
+      <div class="bars"></div>
+    </div>
+  </div>
+  
+  <form>
+    <div class="labels">
+      <div class="row">
+        <h2>What's your mom call you?</h2>
+        <div class="medium-4 medium-offset-4 small-10 small-offset-1">
+          <input id="name" type="text" placeholder="" />
+        </div>
+      </div>
+
+      <div class="space"></div>
+    
+      <div class="row">
+          <h2>Enter your phone no.</h2>
+          <div class="medium-4 medium-offset-4 small-10 small-offset-1">
+            <input id="phone" type="text" placeholder="" />
+          </div>
+      </div>
+      
+      <input type="submit" class="button" id="activate" value="Activate" />
+      
+    </div>
+  </form>
+  
+  
+  <div class="row">
+    <div class="small-12 columns">
+      <h1 id="bottom-safe">Is this safe?</h1>
+    </div>
+  </div>
+
+  <!-- body content here -->
+
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+</div>
+</body>
+  <style>
+    body {
+      background-color: #23272b;
+    }
+  </style>
+</html>
